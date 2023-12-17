6fa7b6145c7f148272efdf652aa63151ae5c606e
geekOnClick <kozloww1508@yandex.ru>
2023-12-17 11:42:02 +0300

    add first changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
+import '../pages/index.css';
+
+import { initialCards } from './cards';
+
 // @todo: Темплейт карточки
 const template = document.querySelector('#card-template').content;
 
@@ -9,22 +13,21 @@ function makeCardNode(elem, removeCard) {
     const card = template.querySelector('.card').cloneNode(true);
     const title = card.querySelector('.card__title');
     const img = card.querySelector('.card__image');
-    
+
     title.textContent = elem.name;
     img.src = elem.link;
     img.alt = elem.name;
-    
+
     const deleteBtn = card.querySelector('.card__delete-button');
     deleteBtn.addEventListener('click', removeCard);
     return card;
-};
+}
 
 // @todo: Функция удаления карточки
 function removeCard(event) {
     event.target.closest('.card').remove();
-  };
+}
 
-// @todo: Вывести карточки на страницу
 function addListTemplate(cards) {
     initialCards.forEach((el) => {
         cardsList.appendChild(makeCardNode(el, removeCard));
@@ -32,4 +35,3 @@ function addListTemplate(cards) {
 }
 
 addListTemplate(initialCards);
-
